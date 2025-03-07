2ccf5832914bf0b2f167d739d5f33e74dd3dffa0
siljeblikeng15 <siljeblikeng15@gmail.com>
2025-03-07 17:12:55 +0000

    Add 2.cpp

---
+++ src/2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  // Declare a variable and initialize it with a value
+  int x = 5;
+
+  // Print the value of the variable to the console
+  cout << "The value of x is: " << x << endl;
+
+  return 0;
+}
