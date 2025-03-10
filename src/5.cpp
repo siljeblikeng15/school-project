ee65ecdf781faa79784ef996d983fdf56d717baa
siljeblikeng15 <siljeblikeng15@gmail.com>
2025-03-10 00:27:36 +0000

    Add 5.cpp

---
+++ src/5.cpp
@@ -0,0 +1,11 @@
+
+int main() {
+    int x = 5;
+    int y = 10;
+    if (x > y) {
+        std::cout << "x is greater than y" << std::endl;
+    } else {
+        std::cout << "x is less than or equal to y" << std::endl;
+    }
+    return 0;
+}
