d53a16479d55a5f2f454300f3b75ef26a962dcc6
siljeblikeng15 <siljeblikeng15@gmail.com>
2025-03-11 00:17:17 +0000

    Add 6.cpp

---
+++ src/6.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int x = 5;
+    while (x > 0) {
+        cout << "The value of x is: " << x << endl;
+        x--;
+    }
+    return 0;
+}
