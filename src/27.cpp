621babcbde2757c2462341cbfe7c76f90eee6af0
siljeblikeng15 <siljeblikeng15@gmail.com>
2025-04-08 19:49:56 +0000

    Add 27.cpp

---
+++ src/27.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    int y = 3;
+    
+    if (x > y) {
+        cout << "X is greater than Y." << endl;
+    } else {
+        cout << "Y is greater than X." << endl;
+    }
+    
+    return 0;
+}
